c974734e67ef6924daf03d76383b7823d53ad60c
Emmanuel Tanimowo <Oluwasegunprosperity@gmail.com>
2023-06-06 11:46:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//generate frequently used html
+document.querySelector('#main-navbar').innerHTML = `<div><h2>CSC 227 <span>Course</span></h2><p>By Tanimowo Emmanuel</p></div><ul>
+<li><a href="index.html">Home</a></li>
+<li><a href="support.html">Support</a></li>
+<li><a href="about.html">About Us</a></li>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="contact.html">Contact Us</a></li></ul>`
